3b714cc6d65186954687525e1d2dac35a4bcc3b8
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-05-24 10:06:18 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
           <div className="box" style={{ height: "40%", paddingLeft: 20 }}>
             <div className="text-box">Ваш проводник к <span className="text-box-span">психо эмоциональному</span> благополучию!</div>
           </div>
-          <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="watch-button d-flex align-items-center" style={{ height: "15%", width: "45%", marginTop: 20 }}>
+          <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="watch-button d-flex align-items-center" style={{ textDecoration: "none", height: "15%", width: "45%", marginTop: 20 }}>
             <Image src={watchSrc} width="10%" height="20%" alt="watch" style={{ marginLeft: "4%" }}></Image>
             <div className="watch-button-text" style={{ marginLeft: 10 }}>Смотреть подкаст</div>
           </Link>
