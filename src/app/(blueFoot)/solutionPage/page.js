8c5c4e979c87a8f6d79240001cbe2fe5c32e114d
bnr-baby <149882116+bnr-baby@users.noreply.github.com>
2024-05-29 17:56:22 +0500

    Add files via upload

--- src/app/(blueFoot)/solutionPage/page.js
+++ src/app/(blueFoot)/solutionPage/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import"./style.css";
+import imgSrc from "../../../../public/app.png";
 
-export default function statisticsPage() {
+export default function solutionPage() {
     return (
-        <>
-            Here
-        </>
+        <div className="body">
+            <h1>MindBridge</h1>
+            <div className="box">
+                <div id="solutionText">
+                    <p>Bla blabla</p>
+                </div>
+                <Image src={imgSrc} id="img" alt="logo"></Image>
+            </div>
+        </div>
     );
 }
