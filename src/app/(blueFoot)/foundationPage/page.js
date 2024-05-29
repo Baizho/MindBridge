6060ea6b8765e8414e395204cc80e8891467a4ea
bnr-baby <149882116+bnr-baby@users.noreply.github.com>
2024-05-29 17:55:33 +0500

    Add files via upload

--- src/app/(blueFoot)/foundationPage/page.js
+++ src/app/(blueFoot)/foundationPage/page.js
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client"
+import "./style.css"
+import { useRouter } from 'next/router'
+import React, { useState } from 'react';
+import Link from "next/link";
 
-export default function statisticsPage() {
+export default function foundationPage() {
+    const [isVisible, setIsVisible] = useState({});
+
+    const handleClick = (id) => {
+        setIsVisible(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
+
     return (
-        <>
-            Here
-        </>
+        <div className="body">
+            <div>
+                <h1> xNiet Foundation</h1>
+            </div>
+            <div className="buttons">
+                <button onClick={() => handleClick('button1')} className="button">
+                    <h3>Information</h3>
+                </button>
+                {isVisible['button1'] && <div id="sInfoPage">
+                    Here will be information about Niet Foundation
+                </div>}
+                <button onClick={() => handleClick('button2')} className="">
+                    <h3>Links</h3>
+                </button>
+                {isVisible['button2'] && <div id="sInfoPage">
+                    Links will
+                </div>}
+            </div>
+        </div>
     );
 }
