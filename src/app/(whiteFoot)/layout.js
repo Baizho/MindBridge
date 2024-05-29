4f83b395f44ad68a77b7657a6015502b1a1820b4
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-05-29 18:58:05 +0500

    upd

--- src/app/(whiteFoot)/layout.js
+++ src/app/(whiteFoot)/layout.js
@@ -1,8 +1,8 @@
 import { Inter, Margarine } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
-import Header from "../../components/Header/Header.js";
 import FooterWhite from "../../components/FooterWhite/FooterWhite.js";
+import Header from "../../components/Header/Header.js";
 // import FooterBlue from "../../components/FooterBlue/FooterBlue";
 
 export default function RootLayout({ children }) {
--- src/components/FooterWhite/FooterWhite.js
+++ src/components/FooterWhite/FooterWhite.js
@@ -20,11 +20,11 @@ export default function FooterWhite() {
                     <div className="d-flex justify-content-center"><div className="lines" style={{ width: "50%" }}></div></div>
                     <div className="d-flex justify-content-center" style={{ marginTop: 20 }}>
                         <Image alt="phonelogo" src={phoneLogoSrc1} width={30} height={30} ></Image>
-                        <div className="number" style={{ marginLeft: 20 }}>8-111-111-1111</div>
+                        <div className="footer-text" style={{ marginLeft: 20 }}>8-111-111-1111</div>
                     </div>
                     <div className="d-flex justify-content-center" style={{ marginTop: 20 }}>
                         <Image alt="maillogo" src={mailLogoSrc1} width={30} height={30}></Image>
-                        <div className="number" style={{ marginLeft: 20 }}>@mindbridge.qz</div>
+                        <div className="footer-text" style={{ marginLeft: 20 }}>@mindbridge.qz</div>
                     </div>
                 </div>
                 <div className="col-3 text-center justify-content-center" style={{ height: "100%", left: "65%", top: "5%", position: "absolute" }}>
@@ -32,7 +32,7 @@ export default function FooterWhite() {
                     <div className="d-flex justify-content-center"><div className="lines" style={{ width: "50%" }}></div></div>
                     <div className="d-flex justify-content-center" style={{ marginTop: 20 }}>
                         <Image alt="instalogo" src={instaLogoSrc1} width={30} height={30}></Image>
-                        <div className="number" style={{ marginLeft: 20 }}>@mindbridge.qz</div>
+                        <div className="footer-text" style={{ marginLeft: 20 }}>@mindbridge.qz</div>
                     </div>
 
                 </div>
