c968e17dc88dd6f093f6c058f86c55415e4e03c6
bnr-baby <149882116+bnr-baby@users.noreply.github.com>
2024-05-29 17:57:50 +0500

    Add files via upload

--- src/app/(whiteFoot)/statisticsPage/page.js
+++ src/app/(whiteFoot)/statisticsPage/page.js
@@ -1,18 +1,35 @@
 import Image from "next/image";
 import "./style.css";
+import arrow from "../../../../public/arrow.png";
 
 export default function statisticsPage() {
     return (
         <>
-            <div className="d-flex justify-content-center" style={{ height: 70, border: "3px solid #293991", borderTop: 0, marginTop: -30 }}>
-                <div className="text-title">Статистика психо-эмоциональных проблем в Казахстане </div>
+            <div className="text-box">
+                <div className="text-title">
+                    Статистика психо-эмоциональных проблем в Казахстане
+                </div>
             </div>
-            <div className="d-flex justify-content-center" style={{ height: 70, marginTop: 30 }}>
-                <div className="text-title">Some statistics..</div>
+            <div className="stats">
+                <div className="content" id="content1">
+                    <h4>1</h4>
+                    <p>There will be some statistics</p>
+                </div>
+                <Image src={arrow} alt="arrow" className="arrow" id="arrow1"></Image>
+                <div className="content" id="content2">
+                    <h4>2</h4>
+                    <p>There will be some statistics</p>
+                </div>
+                <Image src={arrow} alt="arrow" className="arrow" id="arrow2"></Image>
+                <div className="content" id="content3">
+                    <h4>3</h4>
+                    <p>There will be some statistics</p>
+                </div>
             </div>
-
-            <div className="d-flex justify-content-center" style={{ marginBottom: "auto", backgroundColor: "#293991", height: 70, marginTop: 30 }}>
-                Here
+            <div className="join">
+                <div className="jtext">
+                    <h4>Присоединяйтесь к нам, на пути к решению!</h4>
+                </div>
             </div>
         </>
     );
