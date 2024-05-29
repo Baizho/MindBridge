79e09568a868c555549bc10daf17971ab8ddb5e0
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-05-29 20:09:13 +0500

    appPage

--- src/app/(blueFoot)/appPage/page.js
+++ src/app/(blueFoot)/appPage/page.js
@@ -1,21 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Lexend_Deca } from "next/font/google";
+import arrowLeft from "../../../../public/Arrow 5.png";
+import arrowRight from "../../../../public/Arrow 6.png"
+import phoneFront from "../../../../public/Purple iphone 14 Pro front.png"
+import phoneBack from "../../../../public/Purple iphone 14 Pro.png"
 import './style.css';
 
 export default function solutionPage() {
     return (
-        <div style={{ marginTop: 30, marginBottom: 30 }}>
-            <div className="d-flex" style={{ height: 500, marginLeft: "5%", marginRight: "5%" }} >
-                <div className="col-6">
-                    <div className="pageHeading" style={{ height: "20%" }}>MindBridge</div>
-                    <div className="box" style={{ height: "80%", paddingLeft: 20 }}>
-                        <div className="text-box">текст о приложение и как оно решает проблему</div>
-                    </div>
+        <div style={{ marginBottom: 30 }}>
+            <div className="app-pageHeading">Прототип приложения</div>
+            <div className="app-pageHeadingLine"></div>
+            <div className="d-flex align-items-center justify-content-center">
+                <Image src={phoneBack} alt="phoneBack" height={350}></Image>
+                <div className="app-arrowLeft">
+                    <Image src={arrowLeft} alt="arrowLeft" height={35}></Image>
                 </div>
-                <div className="col-6">
-                    <div className="box bg-dark-subtle" style={{ height: "100%", marginLeft: "10%" }}></div>
+                <Image src={phoneFront} alt="phoneFront" height={400}></Image>
+                <div className="d-flex app-arrowRight">
+                    <Image src={arrowRight} alt="arrowRight" height={35}></Image>
                 </div>
-            </div >
+                <Image src={phoneBack} alt="phoneBack" height={350}></Image>
+            </div>
+            <div className="app-pageInstruction">До выхода приложения, осталось: </div>
+            <div className="app-pageInstructionLine"></div>
+            <div className="d-flex justify-content-center">
+                <div className="d-flex align-items-center justify-content-center app-pageCountDown">
+                    <div className="app-countDownText">обратный отсчет до выходп приложения</div>
+                </div>
+            </div>
         </div>
     );
 }
