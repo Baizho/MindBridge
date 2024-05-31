da630760bc9f69c12a7b1aed2e434954f65cdd68
bnr-baby <Bekbergen.Nabikhan@astanaboysbil.edu.kz>
2024-05-31 19:16:32 +0500

    Foundation

--- src/app/(blueFoot)/foundationPage/page.js
+++ src/app/(blueFoot)/foundationPage/page.js
@@ -5,33 +5,52 @@ import React, { useState } from 'react';
 import Link from "next/link";
 
 export default function foundationPage() {
-    const [isVisible, setIsVisible] = useState({});
+    const [isVisible, setIsVisible] = useState({
+        'button1': true, // This will make the text for button1 visible by default
+        'button2': false
+    });
 
     const handleClick = (id) => {
-        setIsVisible(prevState => ({
-            ...prevState,
-            [id]: !prevState[id]
-        }));
+        // Reset all keys in the isVisible object to false
+        const resetVisibility = Object.keys(isVisible).reduce((acc, key) => {
+            acc[key] = false;
+            return acc;
+        }, {});
+
+        setIsVisible({
+            ...resetVisibility,
+            // Then set the clicked button's visibility to the opposite of its current state
+            [id]: !isVisible[id]
+        });
     };
+
+    // ... rest of your code
 
     return (
         <div className="body">
-            <div>
-                <h1> xNiet Foundation</h1>
+            <div  className="foundHead">
+                <div> x<span id="niet">Niet</span> <span id="found">Foundation</span></div>
             </div>
             <div className="buttons">
-                <button onClick={() => handleClick('button1')} className="button">
-                    <h3>Information</h3>
-                </button>
-                {isVisible['button1'] && <div id="sInfoPage">
-                    Here will be information about Niet Foundation
-                </div>}
-                <button onClick={() => handleClick('button2')} className="">
-                    <h3>Links</h3>
-                </button>
-                {isVisible['button2'] && <div id="sInfoPage">
-                    Links will
-                </div>}
+
+                <div className="button">
+                    <button onClick={() => handleClick('button1')} className="button">
+                        <p>Information</p>
+                    </button>
+                    <button onClick={() => handleClick('button2')} className="">
+                        <p>News</p>
+                    </button>
+                </div>
+                <div className="info">
+
+                    {isVisible['button1'] && <div id="sInfoPage">
+                        Here will be information about Niet Foundation
+                    </div>}
+                    {isVisible['button2'] && <div id="sInfoPage">
+                        News about Niet Foundation
+                    </div>}
+                </div>
+
             </div>
         </div>
     );
