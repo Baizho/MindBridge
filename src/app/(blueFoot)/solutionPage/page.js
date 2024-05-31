566c34f346b734810ac457f265e706135f2b1a5f
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-05-31 19:49:59 +0500

    upd

--- src/app/(blueFoot)/solutionPage/page.js
+++ src/app/(blueFoot)/solutionPage/page.js
@@ -8,7 +8,7 @@ export default function solutionPage() {
             <div className="d-flex" style={{ height: 500, marginLeft: "5%", marginRight: "5%" }} >
                 <div className="col-6">
                     <div className="pageHeading" style={{ height: "20%", marginBottom: "5%", }}>MindBridge</div>
-                    <div className="box" style={{ height: "72.5%", paddingLeft: 20 }}>
+                    <div className="box" style={{ height: "72.5%" }}>
                         <div className="text-box">Наше приложение создано для улучшения психо-эмоционального состояния школьников. <span className="text-box-span">C MindBridge вы сможете легко отслеживать и анализировать своё состояние c помощью регулярных ментальных проверок и тестов.</span></div>
                     </div>
                 </div>
